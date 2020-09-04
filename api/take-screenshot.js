cf0cd7a0cc443609cf7eacd2f1a270da5eed22bd
jacksandeman <jack.sandeman@pragmatic.agency>
2020-09-04 14:32:21 +0100

    Screenshot api test

--- api/take-screenshot.js
+++ api/take-screenshot.js
@@ -1,4 +1,25 @@
-const { sendWebScreenshot } = require('../tools/screenshot');
+const screenshotApiClient = require('screenshotapi.net')('9TAV8BGMFQ4VH8XWPYNRCYPQQO7DYQGC')
 
-export default sendWebScreenshot;
+export default async function takeScreenshot(req, res) {
+    const url = req.query.url;
+    const screenshotData = req.body;
 
+    let screenshotUrl = await screenshotApiClient
+        .getUrlOfScreenshot({
+            url: url,
+            fresh: true,
+            width: 1920,
+            height: 1080,
+            full_page: true,
+            lazy_load: true,
+        })
+
+    let screenshotJson = {
+        "url": req.query.url,
+        "deviceName": screenshotData.deviceName,
+        "deviceType": screenshotData.deviceType,
+        "screenshot": screenshotUrl
+    };
+
+    res.json(screenshotJson)
+}
